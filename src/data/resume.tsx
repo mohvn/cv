9ff7b436cd101184f42f8c9decf8cf654667601d
Mohan Elias <mohan.elias@proton.me>
2024-11-12 11:01:53 -0300

    fix: trying to fix images

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -1,6 +1,5 @@
 import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
-import { prefix } from "@/utils/prefix";
 
 export const DATA = {
   name: "Mohan Elias",
@@ -12,7 +11,7 @@ export const DATA = {
     "Desenvolvedor de Software motivado a aplicar minha base técnica e habilidades de resolução de problemas para criar soluções digitais impactantes",
   summary:
     "Apaixonado por tecnologia e com um forte desejo de contribuir para o meu avanço. Sou **estudante de Análise e Desenvolvimento de Sistemas**, mas já carrego comigo uma bagagem de experiências práticas que me permitem ir além da teoria. Como **Freelancer**, desde cedo, busquei colocar meu conhecimento em prática. Atuei, e atuo como freelancer em diversos projetos, desenvolvendo websites e aplicações web completas, desde a criação da interface até a implementação de funcionalidades complexas.",
-  avatarUrl: `${prefix}/static/images/me.png`,
+  avatarUrl: "/static/images/me.png",
   skills: [
     "React",
     "Next.js",
@@ -61,7 +60,7 @@ export const DATA = {
       badges: [],
       location: "Remoto",
       title: "Desenvolvedor de Software",
-      logoUrl: `${prefix}/static/images/freelancer.png`,
+      logoUrl: "/static/images/freelancer.png",
       start: "Jan 2021",
       end: "Atual",
       description:
@@ -73,7 +72,7 @@ export const DATA = {
       badges: [],
       location: "Presencial",
       title: "Técnico Help Desk",
-      logoUrl: `${prefix}/static/images/bnx.png`,
+      logoUrl: "/static/images/bnx.png",
       start: "Jun 2023",
       end: "Atual",
       description:
@@ -85,7 +84,7 @@ export const DATA = {
       badges: [],
       location: "Presencial",
       title: "Atendimento ao Cliente",
-      logoUrl: `${prefix}/static/images/brasilsorriso.png`,
+      logoUrl: "/static/images/brasilsorriso.png",
       start: "Abr 2022",
       end: "Jun 2023",
       description:
@@ -97,7 +96,7 @@ export const DATA = {
       school: "Universidade Católica de Santos",
       href: "https://www.unisantos.br/",
       degree: "Graduação em Análise e Desenvolvimento de Sistemas",
-      logoUrl: `${prefix}/static/images/unisantos.png`,
+      logoUrl: "/static/images/unisantos.png",
       start: "2023",
       end: "2024",
     },
@@ -105,7 +104,7 @@ export const DATA = {
       school: "ETEC - Escola Técnica Estadual de São Paulo",
       href: "https://etecsp.cps.sp.gov.br/",
       degree: "Técnico em Administração",
-      logoUrl: `${prefix}/static/images/etec.png`,
+      logoUrl: "/static/images/etec.png",
       start: "2021",
       end: "2022",
     },
@@ -113,7 +112,7 @@ export const DATA = {
       school: "CCAA",
       href: "https://ccaa.com.br/",
       degree: "Inglês",
-      logoUrl: `${prefix}/static/images/ccaa.png`,
+      logoUrl: "/static/images/ccaa.png",
       start: "2011",
       end: "2019",
     },
@@ -141,7 +140,7 @@ export const DATA = {
           icon: <Icons.globe className="size-3" />,
         },
       ],
-      image: `${prefix}/static/images/sistemaabelha.png`,
+      image: "/static/images/sistemaabelha.png",
       video: "",
     },
     {
@@ -155,7 +154,7 @@ export const DATA = {
         "PHP",
         "Bootstrap",
       ],
-      image: `${prefix}/static/images/lojaodobras.png`,
+      image: "/static/images/lojaodobras.png",
       links: [],
     },
     {
@@ -182,7 +181,7 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: `${prefix}/static/images/sdswiki.png`,
+      image: "/static/images/sdswiki.png",
     },
     {
       title: "sttp - Página Inicial Dinâmica",
@@ -208,7 +207,7 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: `${prefix}/sttp.png`,
+      image: "/static/images/sttp.png",
     },
   ],
   hackathons: [
@@ -218,7 +217,7 @@ export const DATA = {
       location: "Santos, SP",
       description:
         "Participei da squad de Dados no desenvolvimento de um sistema de monitoramento de Outliers dos indicadores ODS para a Prefeitura Municipal de Santos. O evento reuniu equipes multidisciplinares (Front-End, Back-End, Dados, UI/UX, DevOps e Metodologias Ágeis) com o desafio de entregar uma solução completa em apenas um dia. O projeto foi concluído com sucesso, demonstrando a importância do trabalho em equipe e da capacidade de resolver problemas sob pressão.",
-      image: `${prefix}/static/images/E.T.I.S.png`,
+      image: "/static/images/E.T.I.S.png",
       mlh: "",
       links: [],
     },
